ebe3faa37ac095742eb4594fb85da2d9bffcc631
Mandvi <mandvishukla20@gmail.com>
2023-04-12 00:44:32 +0530

    Accordion for prevriew form

--- api/routes/blogs.js
+++ api/routes/blogs.js
@@ -4,7 +4,7 @@ const router = express.Router();
 //Require controller modules 
 const blogsControllers = require('../controllers/blogsControllers');
 
-router.get('/', blogsControllers.index);
+// router.get('/', blogsControllers.index);
 router.post('/', blogsControllers.insert);
 // router.put('/update/:id', blogsControllers.update);
 // router.delete('/delete/:id', blogsControllers.delete);
