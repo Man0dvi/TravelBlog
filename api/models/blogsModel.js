68c3259102ffc04491a50721c79021a7a330744e
Mandvi <mandvishukla20@gmail.com>
2023-04-12 23:26:25 +0530

    Updated files and added blog component with static fields

--- api/models/blogsModel.js
+++ api/models/blogsModel.js
@@ -3,13 +3,20 @@ const mongoose = require('mongoose')
 const blogsSchema = new mongoose.Schema({
     placename: String,
     cityname: String,
-    countryname: String
+    countryname: String,
+    // image:{type:String},
+    budget: String,
+    todo:String,
+    transport:String,
+    stay:String,
+    description:String,
+    blogTitle:String,
     
 })
 // blogsSchema.methods.insert = function insert() {
 
 // }
 
-const blogss = mongoose.model('blogss', blogsSchema)
+const blogs = mongoose.model('blogs', blogsSchema)
 
-module.exports = {blogss}
+module.exports = {blogs}
